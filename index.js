5172c7e462fcbf452e868bf76e3a1056d301ff8e
thyoon515 <thyoon515@gmail.com>
2022-04-09 22:48:05 -0400

    commit tree 3b630f5f32a457a9e5c78c9df6a956ecb4114fa1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("walk the dogs");
+
+function mondayWork(work = "go to the office"){
+    return `This Monday, I will ${work}.`;
+}
+mondayWork();
+mondayWork("code");
+
+function wrapAdjective(highlight = "*") {
+    return function(something = "a hard worker") {
+        return `You are ${highlight}${something}${highlight}!`
+    }
+}
+wrapAdjective("||")("a dedicated programmer");
